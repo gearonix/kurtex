ee6c859f3111ed2fe806ee990ffb013cd6c25c17
Gearonix <egor.uzhanin@gmail.com>
2023-11-09 13:31:59 +0300

    chore: initialized packages

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,10 @@
+const { composePlugins, withNx } = require('@nx/next')
+const nextConfig = {
+  nx: {
+    svgr: false
+  }
+}
+
+const plugins = [withNx]
+
+module.exports = composePlugins(...plugins)(nextConfig)
