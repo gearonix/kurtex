3e880cfdfa2021458b6a56e826e6c072d5095d3b
Gearonix <egor.uzhanin@gmail.com>
2023-11-09 15:43:44 +0300

    feat(frontend): registered pwa, service-worker, added robots.txt

---
+++ packages/frontend/src/app/entrypoint/next.config.mjs
@@ -0,0 +1,28 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+import withPWA from 'next-pwa'
+import { composePlugins, withNx } from '@nx/next'
+
+const isDev = process.env.NODE_ENV = 'development'
+
+const nextConfig = {
+  nx: {
+    svgr: false
+  }
+}
+
+const plugins = [
+  withNx,
+  withBundleAnalyzer,
+  [
+    withPWA,
+    {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+      disable: isDev,
+      sw: '/public/service-worker.js'
+    }
+  ]
+]
+
+module.exports = composePlugins(...plugins)(nextConfig)
